9d441b52dce89d73e3e935e716555dd27e66352d
Luke Kuza <admin@dark-programs.com>
2012-08-21 11:36:16 -0400

    Support more carriers for MEID/ESN check

--- src/com/darkprograms/esn/checkesn/carrier/Carrier.java
+++ src/com/darkprograms/esn/checkesn/carrier/Carrier.java
@@ -2,6 +2,7 @@ package com.darkprograms.esn.checkesn.carrier;
 
 /**
  * Carrier class containing carrier constants
+ *
  * @author Luke Kuza
  */
 public class Carrier {
@@ -10,5 +11,37 @@ public class Carrier {
      * Sprint Carrier Constant used in checkesn Request
      */
     public final static String SPRINT = "sprint";
+    /**
+     * Verizon Carrier Constant used in checkesn Request
+     */
+    public final static String VERIZON = "verizon";
+    /**
+     * MetroPCS Carrier Constant used in checkesn Request
+     */
+    public final static String METROPCS = "metropcs";
+    /**
+     * Straight Talk Carrier Constant used in checkesn Request
+     */
+    public final static String STRAIGHT_TALK = "straight";
+    /**
+     * Net10 Carrier Constant used in checkesn Request
+     */
+    public final static String NET10 = "net10";
+    /**
+     * Tracfone Carrier Constant used in checkesn Request
+     */
+    public final static String TRACFONE = "tracfone";
+    /**
+     * Cricket Carrier Constant used in checkesn Request
+     */
+    public final static String CRICKET = "cricket";
+    /**
+     * PlatinumTel Carrier Constant used in checkesn Request
+     */
+    public final static String PLATINUM_TEL = "platinumtel";
+    /**
+     * iWireless Carrier Constant used in checkesn Request
+     */
+    public final static String IWIRELESS = "iwireless";
 
 }
